e67855650ca2eaca50384e08eb4e27e481f19d56
yakimka <ss.yakim@gmail.com>
2022-05-29 22:05:57 +0300

    Add help text for message_template

--- server/db/admin.py
+++ server/db/admin.py
@@ -18,14 +18,30 @@ def load_configuration() -> dict:
         return json.load(fp)
 
 
-def get_choices(type):  # noqa: PLW0622
+def get_choices(type) -> list[tuple[str, str]]:  # noqa: PLW0622
     config = load_configuration()
     return [(item["type"], item["type"]) for item in config["handlers"][type]]
 
 
-def fields_config(type):  # noqa: PLW0622
+def fields_config(type) -> dict:  # noqa: PLW0622
     config = load_configuration()
     return {item["type"]: item["options"] for item in config["handlers"][type]}
+
+
+def message_template_help_text() -> str:
+    config = load_configuration()
+    fields_mapping = {}
+
+    for item in config["handlers"]["parsers"]:
+        if fields := item.get("return_fields_schema"):
+            fields_mapping[item["type"]] = list(fields)
+    parts = ["Available fields:"]
+    parts.extend(
+        f"<b>{key}:</b> {', '.join(fields)}"
+        for key, fields in fields_mapping.items()
+    )
+
+    return "<br>".join(parts)
 
 
 class SourceAdminForm(ModelForm):
@@ -64,6 +80,10 @@ class SourceAdmin(admin.ModelAdmin):
 class ReceiverAdminForm(ModelForm):
     type = ChoiceField(choices=partial(get_choices, "receivers"))
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["message_template"].help_text = message_template_help_text()
+
     class Meta:
         model = models.Receiver
         fields = "__all__"
@@ -89,3 +109,10 @@ class StreamAdmin(admin.ModelAdmin):
     list_display = ("__str__", "source", "receiver", "active")
     ordering = ("source", "receiver")
     list_select_related = ("source", "receiver")
+
+    def get_form(self, request, obj=None, change=False, **kwargs):
+        form = super().get_form(request, obj, change, **kwargs)
+        form.base_fields[
+            "message_template"
+        ].help_text = message_template_help_text()
+        return form
